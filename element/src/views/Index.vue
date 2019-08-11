35426741f96bd3d6da5cf4fb4360c53ba72fbb55
lx812833 <lx812833@163.com>
2019-08-11 20:56:32 +0800

    添加资金列表页面

--- element/src/views/Index.vue
+++ element/src/views/Index.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="index">
     <HeadNav />
-    <router-view></router-view>
+    <LeftNav />
+    <div class="rightContainer">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
 import HeadNav from "@/components/HeadNav.vue";
+import LeftNav from "@/components/LeftMenu.vue";
 export default {
   name: "index",
   components: {
-    HeadNav
+    HeadNav,
+    LeftNav
   }
 };
 </script>
@@ -19,5 +24,13 @@ export default {
   width: 100%;
   height: 100%;
   overflow: hidden;
+  .rightContainer {
+    position: relative;
+    top: 0;
+    left: 180px;
+    width: calc(100% - 180px);
+    height: calc(100% - 71px);
+    overflow: auto;
+  }
 }
 </style>
